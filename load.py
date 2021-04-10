af739d6daad124cdbd1814f717a3ab5a2167ffef
gabcodedev <57406135+gabcodedev@users.noreply.github.com>
2021-04-10 19:13:16 +0200

    Update load.py

--- load.py
+++ load.py
@@ -1,6 +1,7 @@
 import re
 import sys
 
+import numpy as np
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
@@ -12,7 +13,13 @@ import contextlib
 
 import transcribe
 
+import subprocess
+
+from subprocess import Popen, PIPE
+
 skip_large_duration_files = True
+
+use_deepspeech = True
 
 file_number = 1
 
@@ -22,7 +29,7 @@ input_file = 'input/' + sys.argv[1]
 sound_file = AudioSegment.from_file(input_file)
 sound_file = sound_file.set_frame_rate(22050)  # don't change this
 sound_file = sound_file.set_channels(1)  # don't change this
-audio_chunks = split_on_silence(sound_file, min_silence_len=750,  # 1000 cuts at 1 second of silence. 500 is 0.5 sec
+audio_chunks = split_on_silence(sound_file, min_silence_len=500,  # 1000 cuts at 1 second of silence. 500 is 0.5 sec
                                 silence_thresh=-40)
 
 for i, chunk in enumerate(audio_chunks):
@@ -53,21 +60,42 @@ for i, chunk in enumerate(audio_chunks):
 
     if skip_large_duration_files:
         if duration < 12:
-            transcription = transcribe.get_large_audio_transcription(out_file)
+            if use_deepspeech:
+                model_file_path = 'deepspeech-0.9.3-models/deepspeech-0.9.3-models.pbmm'
+
+                scorer_file_path = 'deepspeech-0.9.3-models/deepspeech-0.9.3-models.scorer'
+
+                os.chdir('deepspeech')
+
+                subprocess.call(
+                    "Scripts\\activate",
+                    shell=True)
+
+                os.chdir('../')
+
+                transcription = subprocess.check_output([
+                    "deepspeech",
+                    '--model', "deepspeech-0.9.3-models/deepspeech-0.9.3-models.pbmm",
+                    "--scorer", "deepspeech-0.9.3-models/deepspeech-0.9.3-models.scorer",
+                    "--audio", out_file], shell=True)
+
+                print(transcription)
+            else:
+                transcription = transcribe.get_large_audio_transcription(out_file)
 
             if transcription != '':
                 if os.path.isfile('output/list.txt'):
                     if os.stat("output/list.txt").st_size != 0:
                         with open('output/list.txt', 'a+') as f:
-                            f.write(f'\nwavs/{file_number}.wav|' + transcription)
+                            f.write(f'\nwavs/{file_number}.wav|' + transcription.decode('utf-8').capitalize().strip()+'.')
                             f.flush()
                     else:
                         with open('output/list.txt', 'a+') as f:
-                            f.write(f'wavs/{file_number}.wav|' + transcription)
+                            f.write(f'wavs/{file_number}.wav|' + transcription.decode('utf-8').capitalize().strip()+'.')
                             f.flush()
                 else:
                     with open('output/list.txt', 'x') as f:
-                        f.write(f'wavs/{file_number}.wav|' + transcription)
+                        f.write(f'wavs/{file_number}.wav|' + transcription.decode('utf-8').capitalize().strip()+'.')
 
                 file_number = file_number + 1
             else:
