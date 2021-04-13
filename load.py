2507582fb3f1dcc3dba83610e3ff9b96f18a14e4
gabcodedev <57406135+gabcodedev@users.noreply.github.com>
2021-04-14 01:37:12 +0200

    Fix big issues with transcription

--- load.py
+++ load.py
@@ -1,3 +1,4 @@
+import math
 import re
 import sys
 
@@ -10,6 +11,8 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 file_number = 1
 
+punctuation = '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
+
 # assign files
 input_file = 'input/' + sys.argv[1]
 
@@ -17,9 +20,7 @@ sound_file = AudioSegment.from_file(input_file)
 sound_file = sound_file.set_frame_rate(22050)  # don't change this
 sound_file = sound_file.set_channels(1)  # don't change this
 
-if len(os.listdir('output/wavs')) == 0:
-    print("Wavs directory is empty")
-else:
+if not len(os.listdir('output/wavs')) == 0:
     list_of_files = glob.glob('output/wavs/*')  # * means all
     latest_file = max(list_of_files, key=os.path.getctime)
 
@@ -36,35 +37,36 @@ transcription = srt
 num_of_sentences = str(transcription).count("'text':")
 
 for sentence in range(num_of_sentences):
-    if transcription[file_number]['text'].strip() != '' or transcription[file_number]['text'] is not None:
-        start_sec = transcription[file_number]['start']
+    if transcription[sentence]['text'].strip() != '' or transcription[sentence]['text'] is not None:
+        start_sec = transcription[sentence]['start']
 
-        duration_sec = transcription[file_number + 1]['start']
-
-        difference_time = duration_sec - start_sec
-
-        end_sec = start_sec + difference_time
+        end_sec = transcription[sentence]['start'] + transcription[sentence]['duration']
 
         # Time to miliseconds
-        startTime = start_sec * 1000
-        endTime = end_sec * 1000
+        startTime = math.trunc(start_sec) * 1000
+        endTime = math.trunc(end_sec) * 1000
 
         extract = sound_file[startTime:endTime]
 
         # Saving
         extract.export('output/wavs/' + str(file_number) + '.wav', format="wav")
 
+        transcriptionstring = transcription[sentence]['text'].capitalize().strip().replace('\n', ' ').strip().rstrip(',').rstrip(';').rstrip('.') + '.'
+
         if os.path.isfile('output/list.txt'):
             if os.stat("output/list.txt").st_size != 0:
                 with open('output/list.txt', 'a+', encoding="utf-8") as f:
-                    f.write(f'\nwavs/{file_number}.wav|' + transcription[file_number]['text'].capitalize().strip().replace("\n", " ") + '.')
+                    f.write(
+                        f'\nwavs/{file_number}.wav|' + transcriptionstring)
                     f.flush()
             else:
                 with open('output/list.txt', 'a+', encoding="utf-8") as f:
-                    f.write(f'wavs/{file_number}.wav|' + transcription[file_number]['text'].capitalize().strip().replace("\n", " ") + '.')
+                    f.write(
+                        f'wavs/{file_number}.wav|' + transcriptionstring)
                     f.flush()
         else:
             with open('output/list.txt', 'x', encoding="utf-8") as f:
-                f.write(f'wavs/{file_number}.wav|' + transcription[file_number]['text'].capitalize().strip().replace("\n", " ") + '.')
+                f.write(
+                    f'wavs/{file_number}.wav|' + transcriptionstring)
 
         file_number = file_number + 1
