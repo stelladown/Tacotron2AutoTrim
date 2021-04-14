e0a5cc638d540f3d9536a3c233b6de0b890faa78
gabcodedev <57406135+gabcodedev@users.noreply.github.com>
2021-04-14 10:37:53 +0200

    Update load.py

--- load.py
+++ load.py
@@ -9,16 +9,23 @@ import os
 
 from youtube_transcript_api import YouTubeTranscriptApi
 
+import youtube_dl
+
 file_number = 1
 
 punctuation = '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
 
-# assign files
-input_file = 'input/' + sys.argv[1]
+youtube_video_link = sys.argv[1]
 
-sound_file = AudioSegment.from_file(input_file)
-sound_file = sound_file.set_frame_rate(22050)  # don't change this
-sound_file = sound_file.set_channels(1)  # don't change this
+if youtube_video_link.count('?v=') > 0:
+    youtube_id = youtube_video_link.split('?v=')[1] # don't change this
+else:
+    youtube_id = youtube_video_link.split('.be/')[1] # don't change this
+
+# create dir if doesn't exist
+os.makedirs(os.path.dirname('input/'), exist_ok=True)
+os.makedirs(os.path.dirname('output/'), exist_ok=True)
+os.makedirs(os.path.dirname('output/wavs/'), exist_ok=True)
 
 if not len(os.listdir('output/wavs')) == 0:
     list_of_files = glob.glob('output/wavs/*')  # * means all
@@ -30,7 +37,25 @@ if not len(os.listdir('output/wavs')) == 0:
     if int(list_of_nums[0]) >= file_number:
         file_number = int(list_of_nums[0]) + 1
 
-srt = YouTubeTranscriptApi.get_transcript("yourvideoid")
+ydl_opts = {'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'outtmpl':'input' + '/%(title)s.%(ext)s',}
+
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([youtube_video_link])
+
+files_in_input = glob.glob('input/*') # * means all if need specific format then *.csv
+latest_input_file = max(files_in_input, key=os.path.getctime)
+
+sound_file = AudioSegment.from_file(latest_input_file)
+sound_file = sound_file.set_frame_rate(22050)  # don't change this
+sound_file = sound_file.set_channels(1)  # don't change this
+
+srt = YouTubeTranscriptApi.get_transcript(youtube_id)
 
 transcription = srt
 
